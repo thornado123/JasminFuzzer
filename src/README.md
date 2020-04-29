18d13ee78e7333c3c541e9edee222c6e73b4ca8c
thornado123 <thor@chora.dk>
2020-04-29 08:29:08 +0200

    New readme and expressions half added to generator

---
+++ src/README.md
@@ -0,0 +1,9 @@
+# Implementation nodes
+
+In the following any shortcomings or bugs in the implementation should be noted
+All the bugs or limiting behaviour should be documented such that it can be 
+added to the report.
+
+## Expressions
+
+1. Signed operations: Has been left out of the initial code (and maybe for good).
--- src/jasminDistribution.py
+++ src/jasminDistribution.py
@@ -10,6 +10,39 @@ def draw_from_dist(dist, seed):
     return values[np.random.choice(np.arange(len(probs)), p=probs)]
 
 
+class Functions:
+
+    def __init__(self, seed):
+        self.seed = seed
+        self.actions = {
+            "pfunbody" : 0.5,
+            "storage"  : 0.1,
+            "stor_type": 0.1,
+            "pvardecl" : 0.3
+        }
+
+        self.sub_actions = {
+
+            "storage" : {
+                "reg": 0.8,
+                "stack": 0.1,
+                "inline": 0.1
+            }
+
+        }
+
+    def get_action(self, sub=None):
+
+        if sub is not None:
+
+            return draw_from_dist(self.sub_actions[sub], self.seed)
+
+        else:
+
+            return draw_from_dist(self.actions, self.seed)
+
+
+
 class Types:
 
     def __init__(self, seed):
@@ -30,7 +63,7 @@ class Types:
                 "u128": 0.1,
                 "u256": 0.05
 
-            }
+            },
 
             "ptype" : {
 
@@ -39,6 +72,73 @@ class Types:
                 "utype": 0.4,
                 "array": 0.2
 
+            }
+
+        }
+
+
+
+    def get_action(self, sub=None):
+
+        if sub is not None:
+
+            return draw_from_dist(self.sub_actions[sub], self.seed)
+
+        else:
+
+            return draw_from_dist(self.actions, self.seed)
+
+
+class Expressions:
+
+    def __init__(self, seed):
+        self.seed = seed
+        self.actions = {
+            "pexpr" : 1.0,
+            "ident" : 0.0,
+            "var"   : 0.0,
+            "prim"  : 0.0,
+            "peop1" : 0.0,
+            "peop2" : 0.0
+        }
+
+        self.sub_actions = {
+
+            "pexpr" : {
+
+                "true"  :
+                "false" :
+                "int"   :
+                "var"   :
+                "array" :
+                "touple":
+                ""
+
+            },
+
+            "peop1" : {
+
+                "!" : 0.5,
+                "-" : 0.5
+
+            },
+
+            "peop2" : {
+
+                "+"     : 0.2,
+                "-"     : 0.2,
+                "*"     : 0.1,
+                "&&"    : 0.05,
+                "&"     : 0.05,
+                "^"     : 0.02,
+                "<<"    : 0.04,
+                ">>"    : 0.04,
+                "=="    : 0.05,
+                "!="    : 0.05,
+                "<"     : 0.05,
+                "<="    : 0.05,
+                ">"     : 0.05,
+                ">="    : 0.05
             }
 
         }
