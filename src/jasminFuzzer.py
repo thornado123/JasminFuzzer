24b687d0138d773e78c4af18cadda7c782c7314f
thornado123 <thor@chora.dk>
2020-05-03 18:46:20 +0200

    Program generation like concatenating lists, only missing variables

--- src/jasminFuzzer.py
+++ src/jasminFuzzer.py
@@ -6,14 +6,15 @@ def main():
     program_generator = JPG.JasminGenerator(1)
     out = program_generator.get_program()
 
+    out = [str(x) for x in  out]
+    print(out)
+    out = "".join(out)
+    print(out)
     with open("/Users/thorjakobsen/GIT/jasmin/compiler/tests/jasminFuzzer/test1.jazz", "w") as file:
 
         file.write(out)
 
         file.close()
-
-    print(out)
-
 
 if __name__ == '__main__':
     main()
