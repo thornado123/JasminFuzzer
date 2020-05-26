5ff6386e9464779b70176c8caecdbbcdbc13b33a
Josef Gharib <jogha94@gmail.com>
2020-05-26 03:14:01 +0200

    com more data

--- src/time_measuring/jazz.s
+++ src/time_measuring/jazz.s
@@ -1,13 +1,17 @@
 	.text
 	.p2align	5
-	.globl	_main_jazz
-	.globl	main_jazz
-_main_jazz:
-main_jazz:
-	movw	$172, %ax
-	cmpw	$42, %ax
-	jbe 	Lmain_jazz$1
-	addq	$42, %rdi
-Lmain_jazz$1:
+	.globl	_f0
+	.globl	f0
+_f0:
+f0:
+	cmpq	$42, %rdi
+	je  	Lf0$1
+	jmp 	Lf0$2
+Lf0$3:
+Lf0$4:
+	jmp 	Lf0$4
+Lf0$2:
+	jne 	Lf0$3
+Lf0$1:
 	movq	%rdi, %rax
 	ret 
